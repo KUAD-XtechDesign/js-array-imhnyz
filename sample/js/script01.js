f1a23712946b5848ed6dff0c811a6fe25a3776d5
imhny <h-kishi2276@st.kyoto-art.ac.jp>
2020-10-30 16:30:19 +0900

    js 01

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -6,7 +6,26 @@ $(function(){
     console.log(students)
   })
 
- 
+  $("#button01").on("click",function(){
+    $("#content").text("最初のメンバーは" + students[0] + "/人数は" + students.length);
+  })
 
+  $("#button02").on("click",function(){
+    $("#content").text(students[2]);
+  })
+
+  $("#button03").on("click",function(){
+    $("#content").text(students[students.lentgh-1]);
+  })
+
+  $("#button04").on("click",function(){
+    if(students[students.length] != "山下"){
+      
+    }
+  })
+
+  $("#button05").on("click",function(){
+    students = students.reverse()
+  })
 
 })
